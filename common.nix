7569d026574081b94f82b72579f315cb3d077a92
Kenny MacDermid <kenny.macdermid@gmail.com>
2021-08-30 18:16:47 -0300

    Harden the chrony service.

--- common.nix
+++ common.nix
@@ -87,6 +87,37 @@ with lib;
     enableNTS = true;
   };
   networking.timeServers = [ "time.nrc.ca" ];
+  # Override systemd config with https://github.com/NixOS/nixpkgs/pull/104944
+  # - hardcode paths, mkforce protectsystem
+  systemd.services.chronyd.serviceConfig = {
+    KeyringMode = "private";
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    NoNewPrivileges = true;
+    PrivateMounts = "yes";
+    PrivateTmp = "yes";
+    ProtectControlGroups = true;
+    ProtectHome = "yes";
+    ProtectHostname = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectSystem = mkForce "strict";
+    ReadWritePaths = [ "/var/run/chrony" "/var/lib/chrony" ];
+    RemoveIPC = true;
+    RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    SystemCallFilter = "@system-service @clock";
+    SystemCallArchitectures = "native";
+
+    # even though in the default configuration chrony does not access the rtc clock,
+    # it may be configured to so so either with the 'rtcfile' configuration option
+    # or using the '-s' flag. so we make sure rtc devices can still be used by it.
+    # at the same time there is no need for chrony to access any other device types.
+    DeviceAllow = "char-rtc";
+    DevicePolicy = "closed";
+  };
 
   ########################################
   # Packages
