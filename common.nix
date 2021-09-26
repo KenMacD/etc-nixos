b19146837702483ef5e6be9f40abffe9b44d149a
Kenny MacDermid <kenny@macdermid.ca>
2021-09-26 13:14:32 -0300

    Use mdns4 for better .local addresses.

--- common.nix
+++ common.nix
@@ -47,8 +47,15 @@ with lib;
   networking.usePredictableInterfaceNames = false;
   services.avahi = {
     enable = true;
-    nssmdns = true;
   };
+  # From https://nixos.wiki/wiki/Printing
+  services.avahi.nssmdns = false; # Use the settings from below
+  # settings from avahi-daemon.nix where mdns is replaced with mdns4
+  system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+  system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+    (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+    (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+  ]);
 
   ########################################
   # User
