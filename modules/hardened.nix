008857a2e929c8dcf2d20a6be6b2b8285b990f2c
Kenny MacDermid <kenny@macdermid.ca>
2022-03-12 16:04:43 -0400

    Set env variables for other allocators.

--- modules/hardened.nix
+++ modules/hardened.nix
@@ -1,8 +1,16 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
 {
+  # set up other allocators for use with LD_PRELOAD
+  environment.variables = {
+    MALLOC_HARDENED = "${pkgs.graphene-hardened-malloc}/lib/libhardened_malloc.so";
+    MALLOC_JEMALLOC = "${pkgs.jemalloc}/lib/libjemalloc.so";
+    MALLOC_SCUDO = "${pkgs.llvmPackages_latest.compiler-rt}/lib/linux/libclang_rt.scudo-x86_64.so";
+    MALLOC_MIMALLOC = "${pkgs.mimalloc}/lib/libmimalloc.so";
+  };
+
   # Some configs pulled from modules/profiles/hardened
   boot.blacklistedKernelModules = [
     # Obscure network protocols
