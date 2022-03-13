eeb53dc24cb4c03ecf1d05eba79a89aa15cc6716
Kenny MacDermid <kenny@macdermid.ca>
2022-03-13 13:04:26 -0300

    Update flake and add pkgs.

--- flake.nix
+++ flake.nix
@@ -4,24 +4,39 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.nixpkgs-staging-next.url = "nixpkgs/staging-next";
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-staging-next }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-staging-next, flake-utils }:
+
     let
       system = "x86_64-linux";
+      overlay-local = self: super: import ./pkgs { pkgs = super; };
       overlay-staging-next = final: prev: {
         staging-next = nixpkgs-staging-next.legacyPackages.${prev.system};
       };
     in {
+      packages.x86_64-linux = import ./pkgs {
+          # TODO: pkgs need overlays?
+          pkgs = import nixpkgs { inherit system;  };
+      };
+
       nixosConfigurations = {
         dn = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit inputs; };
           modules = [
             ({ config, pkgs, ... }: {
-              nixpkgs.overlays = [ overlay-staging-next ];
+              nixpkgs.overlays = [
+                overlay-staging-next
+                overlay-local
+              ];
             })
             # Add to regsitry so nixpkgs commands use system versions
-            ({ pkgs, ... }: { nix.registry.nixpkgs.flake = inputs.nixpkgs; })
+            ({ pkgs, ... }: {
+              nix.registry.nixpkgs.flake = inputs.nixpkgs;
+              nix.registry.local.flake = self;
+            })
             ./common.nix
+            ./modules/nrf52.nix
             ./hosts/dn/configuration.nix
             ./hosts/dn/hardware.nix
             ./modules/hardened.nix
