c1edd77a1c683f42f024375e912ed917a53c1747
Kenny MacDermid <kenny@macdermid.ca>
2022-03-19 22:11:50 -0300

    Add access to the latest packages.

--- flake.nix
+++ flake.nix
@@ -3,16 +3,20 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.nixpkgs-staging-next.url = "nixpkgs/staging-next";
+  inputs.nixpkgs-master.url = "nixpkgs/master";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-staging-next, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-staging-next, nixpkgs-master, flake-utils }:
 
     let
       system = "x86_64-linux";
       overlay-local = self: super: import ./pkgs { pkgs = super; };
       overlay-staging-next = final: prev: {
         staging-next = nixpkgs-staging-next.legacyPackages.${prev.system};
+      };
+      overlay-master = final: prev: {
+        master = nixpkgs-master.legacyPackages.${prev.system};
       };
     in {
       packages.x86_64-linux = import ./pkgs {
@@ -28,11 +32,13 @@
               nixpkgs.overlays = [
                 overlay-staging-next
                 overlay-local
+                overlay-master
               ];
             })
             # Add to regsitry so nixpkgs commands use system versions
             ({ pkgs, ... }: {
               nix.registry.nixpkgs.flake = inputs.nixpkgs;
+              nix.registry.nixpkgs-master.flake = inputs.nixpkgs-master;
               nix.registry.local.flake = self;
             })
             ./common.nix
