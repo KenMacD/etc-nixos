5c5f8f4ce56f14c6db3068fd65cff6edf6076448
Kenny MacDermid <kenny@macdermid.ca>
2022-04-10 18:37:57 -0300

    Update sway debug override version.

--- overlays/sway-dbg.nix
+++ overlays/sway-dbg.nix
@@ -3,51 +3,59 @@ self: super:
 let
   overridePackage = package: override: overrideAttrs:
     (package.override override).overrideAttrs overrideAttrs;
-  swayCommit = "440d0bc22d57b8b0b21a8acbf127243b8d08cfae";
-  wlrootsCommit = "aaf787ee5650e77f0bda4dea8e3ba8325e0e6b39";
+  swayCommit = "8f036b6f788e45a36d3126a661913dd38008cc41";
+  swayHash =
+    "sha256-FfLHD/hhel/42mUz47k5VuXp+Sr6V/tPYM+XZgMn3oI="; # super.lib.fakeSha256;
+  wlrootsCommit = "2e14bed9f790c29146b0eee70eab7d8c704876e9";
+  wlrootsHash =
+    "sha256-FlxBo7wgvLryC+OcvIBiKyk7i0O1/WyVs5UZdU6iXfE="; # super.lib.fakeSha256;
   stdenvDebug = super.stdenvAdapters.keepDebugInfo super.pkgs.clang13Stdenv;
+  mesonFlags = [ ];
+  #  mesonFlags = [
+  #    "-Db_sanitize=memory"
+  #    "-Dc_args=[-fsanitize-memory-track-origins=2,-fno-omit-frame-pointer]"
+  #    "-Db_lundef=false"
+  #  ];
+  #  mesonFlags = [
+  #    "-Db_sanitize=address"
+  #    "-Db_lundef=false"
+  #  ];
 in let
   waylandDbgFrm = super:
-    overridePackage super.wayland {
-      stdenv = stdenvDebug;
-    } (old: {
-      mesonBuildType = "debug";
-      mesonFlags = old.mesonFlags or [ ]
-        ++ [ "-Db_sanitize=address" "-Db_lundef=false" ];
-    });
+    overridePackage super.wayland { stdenv = stdenvDebug; }
+    (old: { mesonBuildType = "debug"; });
+in let
+  wlroots = overridePackage super.wlroots {
+    stdenv = stdenvDebug;
+    wayland = waylandDbgFrm super;
+  } (old: {
+    version = wlrootsCommit;
+    src = super.fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "wlroots";
+      repo = "wlroots";
+      rev = wlrootsCommit;
+      sha256 = wlrootsHash;
+    };
+    buildInputs = old.buildInputs ++ [ super.pkgs.pcre2 ];
+    mesonBuildType = "debug";
+    mesonFlags = old.mesonFlags or [ ] ++ mesonFlags;
+  });
 in {
-      wlroots = overridePackage super.wlroots {
-        stdenv = stdenvDebug;
-        wayland = waylandDbgFrm super;
-      } (old: {
-        version = wlrootsCommit;
-        src = super.fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "wlroots";
-          repo = "wlroots";
-          rev = wlrootsCommit;
-          sha256 = "sha256-5cIO7ctznUrN8Dh1PbmQFMcF1gv5RaYgi8yHBrT+FJ4="; # super.lib.fakeSha256;
-        };
-        buildInputs = old.buildInputs ++ [ super.pkgs.pcre2 ];
-        mesonBuildType = "debug";
-        mesonFlags = old.mesonFlags or [ ]
-          ++ [ "-Db_sanitize=address" "-Db_lundef=false" ];
-      });
-      sway-unwrapped = overridePackage super.sway-unwrapped {
-        stdenv = stdenvDebug;
-        wayland = waylandDbgFrm super;
-      } (old: {
-        version = swayCommit;
-        src = super.fetchFromGitHub {
-          owner = "swaywm";
-          repo = "sway";
-          rev = swayCommit;
-          sha256 = "sha256-ZtIecq86v3g4wT+57eixk4+Me86gYx+mfarurNnl2gE="; # super.lib.fakeSha256;
-        };
-#        patches = (old.patches or [ ]) ++ [ ./sway-ld-preload.patch ];
-        buildInputs = old.buildInputs ++ [ super.pkgs.pcre2 ];
-        mesonBuildType = "debug";
-        mesonFlags = old.mesonFlags or [ ]
-          ++ ["-Db_sanitize=address" "-Db_lundef=false" ];
-      });
-    }
+  sway-unwrapped = overridePackage super.sway-unwrapped {
+    stdenv = stdenvDebug;
+    wayland = waylandDbgFrm super;
+    wlroots = wlroots;
+  } (old: {
+    version = swayCommit;
+    src = super.fetchFromGitHub {
+      owner = "swaywm";
+      repo = "sway";
+      rev = swayCommit;
+      sha256 = swayHash;
+    };
+    buildInputs = old.buildInputs ++ [ super.pkgs.pcre2 ];
+    mesonBuildType = "debug";
+    mesonFlags = old.mesonFlags or [ ] ++ mesonFlags;
+  });
+}
