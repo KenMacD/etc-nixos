5eeed849ea9a994cc5968a7ea5fcbeb5b3d7971e
Kenny MacDermid <kenny@macdermid.ca>
2022-04-10 18:36:43 -0300

    Switch back to zen kernel for 5.17 update.

--- hosts/dn/configuration.nix
+++ hosts/dn/configuration.nix
@@ -43,12 +43,12 @@
       ];
     };
   };
+  services.hardware.bolt.enable = true;
   # Force intel vulkan driver to prevent software rendering:
   environment.variables.VK_ICD_FILENAMES =
     "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/intel_icd.i686.json";
 
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.zfs.enableUnstable = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernel.sysctl = {
     "kernel.sysrq" = 1;
   };
