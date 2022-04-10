d422daaf92c6944134f94045256c2ffbba558d9d
Kenny MacDermid <kenny@macdermid.ca>
2022-04-10 18:16:25 -0300

    Add wpantund packages.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,10 +2,10 @@
 
 with pkgs;
 
-let
-  packages = self:
+let packages = self:
   let callPackage = newScope self;
   in {
+    wpantund = callPackage ./wpantund{};
     zephyr-toolchain = callPackage ./zephyr-toolchain {};
     };
 in lib.fix (lib.extends overrides packages)
