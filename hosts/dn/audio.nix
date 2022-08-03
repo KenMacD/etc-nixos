28d65d13fa7c4994ddcbbda81ec04855245ef7d2
Kenny MacDermid <kenny@macdermid.ca>
2022-08-02 21:07:55 -0300

    Add allowed-rates to hopefully reduce conversions.

--- hosts/dn/audio.nix
+++ hosts/dn/audio.nix
@@ -38,6 +38,13 @@
     };
     jack.enable = true;
     pulse.enable = true;
+    config.pipewire = {
+      "context.properties" = {
+        "default.clock.rate" = 44100;
+        "default.clock.allowed-rates" =
+          [ 44100 48000 88200 96000 176400 192000 384000 ];
+      };
+    };
   };
 
   ########################################
