479f3fc6eda028b2cc6051cf01dc1d9802499f26
Kenny MacDermid <kenny@macdermid.ca>
2022-10-10 19:50:38 -0300

    Remove package list to avoid name issues.

--- common.nix
+++ common.nix
@@ -20,13 +20,6 @@ with lib;
   # Include current config:
   environment.etc.current-nixos-config.source = ./.;
 
-  # Include a full list of installed packages
-  environment.etc.current-system-packages.text = let
-    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
-    sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.unique packages);
-    formatted = builtins.concatStringsSep "\n" sortedUnique;
-  in formatted;
-
   ########################################
   # Hardware
   ########################################
