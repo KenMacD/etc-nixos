5612d20f9a5e9a52aa5d0126cec5873fc55b52d4
Kenny MacDermid <kenny@macdermid.ca>
2022-11-01 17:06:14 -0300

    Update sbctl signing of boot images.

--- hosts/dn/sboot.nix
+++ hosts/dn/sboot.nix
@@ -3,7 +3,8 @@
   # Not great, but okay enough for now:
   system.activationScripts.signbl = lib.stringAfter [ "etc" ]
   ''
-    ${pkgs.sbctl}/bin/sbctl sign-all
+    ${pkgs.sbctl}/bin/sbctl sign-all 2>/dev/null || true
+    for f in /boot/EFI/nixos/*-linux-*-bzImage.efi; do ${pkgs.sbctl}/bin/sbctl sign $f; done
   '';
 
   environment.systemPackages = with pkgs; [
