c29c8ed4a8f5006176e75bbb7e4bcd71797a2e97
Kenny MacDermid <kenny@macdermid.ca>
2022-11-27 00:51:31 -0400

    Fix nix-alien install.
    If a key already exists it is ignored by 'with', so rather than
    have two nix-alien values pass the inputs variable instead.

--- modules/nix-alien.nix
+++ modules/nix-alien.nix
@@ -1,10 +1,10 @@
-{ lib, pkgs, nix-alien, ... }:
+{ lib, pkgs, inputs, ... }:
 
 with lib;
 
 {
   programs.nix-ld.enable = true;
-  environment.systemPackages =  with pkgs; with nix-alien.packages.${system}; [
+  environment.systemPackages =  with pkgs; with inputs.nix-alien.packages.${system}; [
     nix-alien
     nix-index
     nix-index-update
