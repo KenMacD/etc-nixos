59b3367309fd4ce3e2d186b38b60646b10ecace0
Kenny MacDermid <kenny@macdermid.ca>
2023-02-22 18:49:21 -0400

    Switch to 6.2 kernel.

--- hosts/dn/configuration.nix
+++ hosts/dn/configuration.nix
@@ -53,7 +53,7 @@
   environment.variables.VK_ICD_FILENAMES =
     "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/intel_icd.i686.json";
 
-  boot.kernelPackages = pkgs.linuxPackages_6_0;
+  boot.kernelPackages = pkgs.linuxPackages_6_2;
   boot.kernel.sysctl = { "dev.i915.perf_stream_paranoid" = 0; };
   boot.extraModulePackages = with config.boot.kernelPackages; [
     turbostat
