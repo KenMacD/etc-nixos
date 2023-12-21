1494f773035cc09a3b5c51e411dd2fd344ef2223
Kenny MacDermid <kenny@macdermid.ca>
2023-12-21 15:06:03 -0400

    Revert pgvecto-rs to 0.1.11
    The latest version doesn't work with immich, so build the older
    version instead.

--- pkgs/pgvecto-rs.nix
+++ pkgs/pgvecto-rs.nix
@@ -14,13 +14,13 @@
   clang_16,
   darwin,
 }: let
-  version = "0.1.13";
+  version = "0.1.11";
 
   src = fetchFromGitHub {
     owner = "KenMacD";
     repo = "pgvecto.rs";
-    rev = "00a3912ffc8d59352887999d27f0b266e6ddd5c5";
-    hash = "sha256-ySbKGjSuxS25RrSBBMFB8y0xGRKyWm7A5FHEKR30+Ok=";
+    rev = "v${version}";
+    hash = "sha256-89ddRospE4HZDwDBxlDIcanHhEZOxa2t0yJVdeq3R9s=";
   };
 
   toolchain = fenix.fromToolchainFile {
@@ -41,16 +41,10 @@ in
 
     pname = "pgvecto-rs";
 
-    postPatch = ''
-      substituteInPlace crates/c/build.rs \
-        --replace "/usr/bin/clang-16" "${clang_16}/bin/clang"
-    '';
-
     cargoLock = {
       lockFile = src + "/Cargo.lock";
       outputHashes = {
         "openai_api_rust-0.1.8" = "sha256-os5Y8KIWXJEYEcNzzT57wFPpEXdZ2Uy9W3j5+hJhhR4=";
-        "std_detect-0.1.5" = "sha256-RwWejfqyGOaeU9zWM4fbb/hiO1wMpxYPKEjLO0rtRmU=";
       };
     };
 
