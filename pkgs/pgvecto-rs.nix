072340368080abbbbcf93f6a05505db2b3fd3a1d
Kenny MacDermid <kenny@macdermid.ca>
2023-12-21 15:42:25 -0400

    Change pgvecto-rs package to use binary build
    The source build is a little too heavy for the server where this
    is running, so for now just use the binary library.

--- pkgs/pgvecto-rs.nix
+++ pkgs/pgvecto-rs.nix
@@ -1,78 +1,45 @@
 {
   lib,
-  fenix,
-  makeRustPlatform,
-  cargo-pgrx,
-  fetchFromGitHub,
-  buildPgrxExtension,
-  postgresql,
-  curl,
-  pkg-config,
-  openssl,
-  zlib,
   stdenv,
-  clang_16,
-  darwin,
-}: let
-  version = "0.1.11";
-
-  src = fetchFromGitHub {
-    owner = "KenMacD";
-    repo = "pgvecto.rs";
-    rev = "v${version}";
-    hash = "sha256-89ddRospE4HZDwDBxlDIcanHhEZOxa2t0yJVdeq3R9s=";
+  fetchurl,
+  dpkg,
+  postgresql,
+}:
+# Modified from: https://github.com/diogotcorreia/dotfiles/blob/nixos/packages/pgvecto-rs.nix
+# Build from source is in git history, but takes multiple gigs of space and is slow.
+let
+  versionHashes = {
+    "14" = "sha256-8YRC1Cd9i0BGUJwLmUoPVshdD4nN66VV3p48ziy3ZbA=";
+    "16" = "sha256-L+57VRFv4rIEjvqExFvU5C9XI7l0zWj9pkKvNE5DP+k=";
   };
-
-  toolchain = fenix.fromToolchainFile {
-    file = src + "/rust-toolchain.toml";
-    sha256 = "sha256-cbsA/sdnWRA63EBChHtJvGGcACj2CxG8FmE+M57xnl0=";
-  };
-
-  rustPlatform = makeRustPlatform {
-    rustc = toolchain;
-    cargo = toolchain;
-  };
+  major = lib.versions.major postgresql.version;
 in
-  buildPgrxExtension.override {
-    inherit rustPlatform;
-    cargo-pgrx = cargo-pgrx.override {inherit rustPlatform;};
-  } rec {
-    inherit postgresql version src;
-
+  stdenv.mkDerivation rec {
     pname = "pgvecto-rs";
+    version = "0.1.11";
 
-    cargoLock = {
-      lockFile = src + "/Cargo.lock";
-      outputHashes = {
-        "openai_api_rust-0.1.8" = "sha256-os5Y8KIWXJEYEcNzzT57wFPpEXdZ2Uy9W3j5+hJhhR4=";
-      };
+    nativeBuildInputs = [dpkg];
+
+    src = fetchurl {
+      url = "https://github.com/tensorchord/pgvecto.rs/releases/download/v${version}/vectors-pg${major}-v${version}-x86_64-unknown-linux-gnu.deb";
+      hash = versionHashes."${major}";
     };
 
-    nativeBuildInputs = [
-      curl
-      pkg-config
-      rustPlatform.bindgenHook
-      clang_16
-    ];
+    unpackCmd = "dpkg -x $src source";
 
-    buildInputs =
-      [
-        curl
-        openssl
-        zlib
-      ]
-      ++ lib.optionals stdenv.isDarwin [
-        darwin.apple_sdk.frameworks.CoreFoundation
-        darwin.apple_sdk.frameworks.IOKit
-        darwin.apple_sdk.frameworks.Security
-        darwin.apple_sdk.frameworks.SystemConfiguration
-      ];
+    dontBuild = true;
+    dontStrip = true;
+
+    installPhase = ''
+      install -D -t $out/lib usr/lib/postgresql/${major}/lib/*.so
+      install -D -t $out/share/postgresql/extension usr/share/postgresql/${major}/extension/*.sql
+      install -D -t $out/share/postgresql/extension usr/share/postgresql/${major}/extension/*.control
+    '';
 
     meta = with lib; {
       description = "Scalable Vector database plugin for Postgres, written in Rust, specifically designed for LLM";
-      homepage = "https://github.com/tensorchord/pgvecto.rs/";
+      homepage = "https://github.com/tensorchord/pgvecto.rs";
       license = licenses.asl20;
       maintainers = with maintainers; [];
-      mainProgram = "pgvecto-rs";
     };
   }
