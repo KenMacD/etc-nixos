31cdb80db8f85b74c6288489b1a531b267dec4fe
Kenny MacDermid <kenny@macdermid.ca>
2024-03-13 18:14:31 -0300

    Bump nix-stable

--- pkgs/immich-go.nix
+++ pkgs/immich-go.nix
@@ -5,16 +5,16 @@
 }:
 buildGoModule rec {
   pname = "immich-go";
-  version = "0.8.9";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "simulot";
     repo = "immich-go";
     rev = "${version}";
-    sha256 = "sha256-aOMVESIEZkjucxB47BayQOujMnR5BGavRiJFKw7pyPI=";
+    sha256 = "sha256-ZY5sfWlrpUpteoCa7bP2A+GUnsTMaXJPSBLqgSANxIQ=";
   };
 
-  vendorHash = "sha256-ap+Bj/MBtCmhj2V86iZYB8AYI6sYPfa6b22LLuW9KoE=";
+  vendorHash = "sha256-02Zbht2YM/YGeJrgyblMI0b3rplhOUNZMZ82M4mZ+8o=";
 
   meta = with lib; {
     description = "An alternative to the immich-CLI command that doesn't depend on nodejs installation. It tries its best for importing google photos takeout archives.";
