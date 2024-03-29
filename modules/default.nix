3ada31695f520ec315ffb39d723af4975bccfc4a
Kenny MacDermid <kenny@macdermid.ca>
2024-03-29 20:16:39 -0300

    Remove content address tests for now

--- modules/default.nix
+++ modules/default.nix
@@ -1,8 +1,6 @@
 {
   # Include modules that do not change anything unless enabled
   imports = [
-    ./content-addressed.nix # Test enable content addresses setup
-
     ./home-wifi.nix # Specific trusted-network config
 
     ./sway-desktop.nix # My sway desktop configuration
