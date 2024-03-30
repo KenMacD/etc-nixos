21b7716c654b421be2d5bb2ea36e427cf3868b34
Kenny MacDermid <kenny@macdermid.ca>
2024-03-30 15:26:06 -0300

    Comment out broken waydroid overlay for now

--- overlays/testing.nix
+++ overlays/testing.nix
@@ -50,15 +50,16 @@ in rec {
       '';
   });
 
-  # Allow waydroid to install from a local android image
-  waydroid = super.waydroid.overridePythonAttrs (old: rec {
-    version = "1.3.4";
-    src = super.fetchFromGitHub {
-      owner = old.pname;
-      repo = old.pname;
-      rev = version;
-      sha256 = "sha256-0GBob9BUwiE5cFGdK8AdwsTjTOdc+AIWqUGN/gFfOqI=";
-    };
-    patches = (old.patches or []) ++ [./waydroid-image-path.patch];
-  });
+# TODO: Does not apply 2024-03-30
+#  # Allow waydroid to install from a local android image
+#  waydroid = super.waydroid.overridePythonAttrs (old: rec {
+#    version = "1.3.4";
+#    src = super.fetchFromGitHub {
+#      owner = old.pname;
+#      repo = old.pname;
+#      rev = version;
+#      sha256 = "sha256-0GBob9BUwiE5cFGdK8AdwsTjTOdc+AIWqUGN/gFfOqI=";
+#    };
+#    patches = (old.patches or []) ++ [./waydroid-image-path.patch];
+#  });
 }
