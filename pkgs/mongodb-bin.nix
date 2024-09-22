7f6084337e51c488d6d73885ef2b969de1043fa7
Kenny MacDermid <kenny@macdermid.ca>
2024-09-22 11:13:06 -0300

    Add binary packages for MongoDB 6 and 7

---
+++ pkgs/mongodb-bin.nix
@@ -0,0 +1,53 @@
+{
+  stdenv,
+  lib,
+  fetchurl,
+  autoPatchelfHook,
+  curl,
+  openssl,
+  version,
+  hash,
+  extraBuildInputs ? [],
+  osTarget ? "ubuntu2204",
+}:
+stdenv.mkDerivation {
+  pname = "mongodb";
+  inherit version;
+
+  src = fetchurl {
+    # https://www.mongodb.com/try/download/community-edition/releases
+    url = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${osTarget}-${version}.tgz";
+    inherit hash;
+  };
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  buildInputs =
+    [
+      stdenv.cc.cc.libgcc
+      curl
+      openssl
+    ]
+    ++ extraBuildInputs;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp bin/mongod $out/bin/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "A scalable, high-performance, open source NoSQL database";
+    homepage = "http://www.mongodb.org";
+    platforms = ["x86_64-linux"];
+    license = licenses.unfree;
+  };
+}
