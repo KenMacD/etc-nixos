e7b2f8eeaa4dbf910daa15bcfe01b6e9cc1b84bb
Kenny MacDermid <kenny@macdermid.ca>
2024-11-18 14:44:07 -0400

    Try out kanata for keyboard remapping

--- hosts/ke/configuration.nix
+++ hosts/ke/configuration.nix
@@ -221,6 +221,21 @@ in {
       tod.driver = pkgs.libfprint-2-tod1-goodix;
     };
     fwupd.enable = true;
+    kanata = {
+      enable = true;
+      keyboards = {
+        default = {
+          config = ''
+            (defsrc
+              caps)
+
+            (deflayermap (default-layer)
+              ;; tap caps lock as caps lock, hold caps lock as left control
+              caps (tap-hold 100 100 caps lctl))
+          '';
+        };
+      };
+    };
     openssh.enable = true;
     pcscd.enable = true;
     restic.backups.efi = {
