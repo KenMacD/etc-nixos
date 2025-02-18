93ee51c1ada6eb03b60a7a78baaa00677b401237
Kenny MacDermid <kenny@macdermid.ca>
2025-02-18 15:19:47 -0400

    Convert sccache to use unix socket
    Services was failing with 'Address in use', probably during upgrades.
    Instead trying out unix sockets to see if that works better.

--- modules/sccache.nix
+++ modules/sccache.nix
@@ -11,8 +11,19 @@ in {
     enable = mkEnableOption "sccache server";
   };
 
+  # Test with: SCCACHE_LOG=trace SCCACHE_SERVER_UDS=/run/user/1001/sccache.sock sccache --show-stats
   config = mkIf cfg.enable {
-    environment.variables.SCCACHE_NO_DAEMON = "1";
+    environment.variables = {
+      SCCACHE_NO_DAEMON = "1";
+      SCCACHE_SERVER_UDS = ''''${XDG_RUNTIME_DIR:-"/run/user/$(id -u)"}/sccache.sock'';
+
+      # CARGO_BUILD_RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+      RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+    };
+
+    environment.systemPackages = [
+      pkgs.sccache
+    ];
 
     systemd.user.services.sccache = {
       description = "Sccache server";
@@ -26,6 +37,7 @@ in {
       environment = {
         SCCACHE_IDLE_TIMEOUT = "0";
         SCCACHE_CACHE_SIZE = "2G";
+        SCCACHE_SERVER_UDS = "%t/sccache.sock";
       };
     };
   };
