5fd6521f067ebeaa261cfb73d5ed9c9fd776bf90
Kenny MacDermid <kenny@macdermid.ca>
2025-04-19 16:03:45 -0300

    Add Rust and RustUp devshell

--- flake.nix
+++ flake.nix
@@ -131,6 +131,47 @@
             ]))
         ];
       };
+      rustup = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          rustup
+          llvmPackages.bintools
+          llvmPackages.clang
+          llvmPackages.lld
+          pkg-config
+          sccache
+
+          # Very commonly needed by crates
+          openssl
+        ];
+        shellHook = ''
+          export RUSTC_WRAPPER="${pkgs.sccache}/bin/sccache"
+        '';
+      };
+      rust = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          alejandra
+          cargo
+          cargo-binutils
+          cargo-expand
+          cargo-flamegraph
+          cargo-generate
+          clippy
+          llvmPackages.bintools
+          llvmPackages.clang
+          llvmPackages.lld
+          pkg-config
+          rustc
+          rustup
+          rust-analyzer
+          sccache
+
+          # Very commonly needed by crates
+          openssl
+        ];
+        shellHook = ''
+          export RUSTC_WRAPPER="${pkgs.sccache}/bin/sccache"
+        '';
+      };
     };
 
     nixosConfigurations = {
