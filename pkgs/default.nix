3a5f9c2f29cc75b6e1634707d398993a9b0c3ec1
Kenny MacDermid <kenny@macdermid.ca>
2025-05-08 08:03:00 -0300

    Remove shh as now in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -48,8 +48,6 @@ in {
 
   resticprofile = callPackage ./resticprofile.nix {};
 
-  shh = callPackage ./shh.nix {};
-
   ttok = python3Packages.callPackage ./ttok.nix {};
 
   tun2proxy = callPackage ./tun2proxy {};
