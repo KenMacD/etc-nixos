29c92c6ef6af946a8b6efb2fe71a7a5617a60374
Kenny MacDermid <kenny@macdermid.ca>
2025-06-02 18:01:11 -0300

    Set nvim snippets to not insert on enter alone

--- common.nix
+++ common.nix
@@ -337,7 +337,7 @@ with lib; {
               ['<C-f>'] = cmp.mapping.scroll_docs(4),
               ['<C-Space>'] = cmp.mapping.complete(),
               ['<C-e>'] = cmp.mapping.abort(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              ['<CR>'] = cmp.mapping.confirm({ select = false }), -- select = false to avoid adding entry on just enter alone
               ['<Tab>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                   cmp.select_next_item()
