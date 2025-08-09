5fa76f59a293f320ce2a5aa6f8e890b7984c9cbe
Kenny MacDermid <kenny@macdermid.ca>
2025-08-09 12:14:43 -0300

    Remove no longer used testing overlays
    The starship patch could be pushed upstream some day, but it's
    no longer important locally.

--- overlays/testing.nix
+++ overlays/testing.nix
@@ -7,39 +7,6 @@ in rec {
     libs = old.libs + ":/run/opengl-driver/lib: " + super.intel-gmmlib.outPath + "/lib";
   });
 
-  notmuch = super.notmuch.overrideAttrs (old: {
-    buildInputs =
-      (old.buildInputs or [])
-      ++ [
-        super.sfsexp
-      ];
-    preCheck =
-      (old.preCheck or "")
-      + ''
-        rm test/T850-git.sh
-      '';
-  });
-
-  postman = super.postman.overrideAttrs (old: {
-    postFixup = ''
-      pushd $out/share/postman
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" postman
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" chrome_crashpad_handler
-      for file in $(find . -type f \( -name \*.node -o -name postman -o -name \*.so\* \) ); do
-        ORIGIN=$(patchelf --print-rpath $file); \
-        patchelf --set-rpath "${super.lib.makeLibraryPath old.buildInputs}:$ORIGIN" $file
-      done
-      popd
-      wrapProgram $out/bin/postman --set PATH ${super.lib.makeBinPath [super.openssl super.xdg-utils]}
-    '';
-  });
-
-  starship = super.starship.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [./starship-aws.patch];
-    # Make build faster:
-    doCheck = false;
-  });
-
   # TODO: Does not apply 2024-03-30
   #  # Allow waydroid to install from a local android image
   #  waydroid = super.waydroid.overridePythonAttrs (old: rec {
