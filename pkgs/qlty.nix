804708bf52490a06e4877d3aabfc805ee4672d2a
Kenny MacDermid <kenny@macdermid.ca>
2025-08-21 14:22:42 -0300

    Add qlty package

---
+++ pkgs/qlty.nix
@@ -0,0 +1,77 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  pkg-config,
+  protobuf,
+  bzip2,
+  libgit2,
+  oniguruma,
+  openssl,
+  xz,
+  zlib,
+  zstd,
+  stdenv,
+  darwin,
+  git,
+}:
+# Modified from: https://github.com/ourgal/snowfall/blob/main/packages/qlty/default.nix
+rustPlatform.buildRustPackage rec {
+  pname = "qlty";
+  version = "v0.567.0";
+  src = fetchFromGitHub {
+    owner = "qltysh";
+    repo = "qlty";
+    rev = "v0.567.0";
+    fetchSubmodules = false;
+    sha256 = "sha256-6gAEthjlHs2gF1plxSiUA249qZyrR/p9jOGegOv32Rg=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    outputHashes = {
+      "duct-0.13.7" = "sha256-Txzn025lWXctujpAnmp6JLyWLw7rhloCV5tCa+KkAlA=";
+    };
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    protobuf
+    git
+  ];
+
+  buildInputs =
+    [
+      bzip2
+      libgit2
+      oniguruma
+      openssl
+      xz
+      zlib
+      zstd
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.CoreFoundation
+      darwin.apple_sdk.frameworks.CoreServices
+      darwin.apple_sdk.frameworks.IOKit
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
+
+  env = {
+    OPENSSL_NO_VENDOR = true;
+    RUSTONIG_SYSTEM_LIBONIG = true;
+    ZSTD_SYS_USE_PKG_CONFIG = true;
+  };
+
+  doCheck = false;
+
+  meta = {
+    description = "Qlty CLI: Universal linting, formatting, maintainability, security scanning, and metrics";
+    homepage = "https://github.com/qltysh/qlty";
+    changelog = "https://github.com/qltysh/qlty/blob/${src.rev}/CHANGELOG.md";
+    license = lib.licenses.bsl11;
+    maintainers = with lib.maintainers; [];
+    mainProgram = "qlty";
+  };
+}
