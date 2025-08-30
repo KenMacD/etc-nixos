77e6debc4a6478ccc0cc4920d161c1d48fe919ca
Kenny MacDermid <kenny@macdermid.ca>
2025-08-29 21:12:56 -0300

    Update Spectrum key

--- hosts/ke/spectrum.nix
+++ hosts/ke/spectrum.nix
@@ -7,6 +7,6 @@
   nix.settings.extra-substituters = ["https://cache.dataaturservice.se/spectrum/"];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "spectrum-os.org-1:rnnSumz3+Dbs5uewPlwZSTP0k3g/5SRG4hD7Wbr9YuQ="
+    "spectrum-os.org-2:foQk3r7t2VpRx92CaXb5ROyy/NBdRJQG2uX2XJMYZfU="
   ];
 }
