6430d6284850365901c35488c9614b24bf1b4f53
Kenny MacDermid <kenny@macdermid.ca>
2025-10-25 17:22:57 -0300

    Bump kernel to 6.17

--- common.nix
+++ common.nix
@@ -103,7 +103,7 @@ with lib; {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = mkDefault true;
 
-  boot.kernelPackages = mkOverride (lib.modules.defaultOrderPriority - 1) pkgs.linuxPackages_6_16;
+  boot.kernelPackages = mkOverride (lib.modules.defaultOrderPriority - 1) pkgs.linuxPackages_6_17;
   security.unprivilegedUsernsClone = mkDefault config.virtualisation.containers.enable;
 
   boot.kernel.sysctl = {};
