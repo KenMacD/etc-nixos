f2c9cd1f0dc58af3ebb5f596bd71277c5223563b
Kenny MacDermid <kenny@macdermid.ca>
2025-11-07 15:09:11 -0400

    Add a couple more npm packages

--- pkgs/node2nix/override.nix
+++ pkgs/node2nix/override.nix
@@ -1,18 +1,22 @@
-{pkgs ? import <nixpkgs> {
-    inherit system;
-}, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_14"}:
-
-let
+{
+  pkgs ?
+    import <nixpkgs> {
+      inherit system;
+    },
+  system ? builtins.currentSystem,
+  nodejs ? pkgs."nodejs_14",
+}: let
   nodePackages = import ./default.nix {
     inherit pkgs system nodejs;
   };
 in
-nodePackages // {
-  "@bytebase/dbhub" = nodePackages."@bytebase/dbhub".override {
-    buildInputs = [
-      pkgs.which
-      pkgs.postgresql
-      pkgs.postgresql.pg_config
-    ];
-  };
-}
+  nodePackages
+  // {
+    "@bytebase/dbhub" = nodePackages."@bytebase/dbhub".override {
+      buildInputs = [
+        pkgs.which
+        pkgs.postgresql
+        pkgs.postgresql.pg_config
+      ];
+    };
+  }
