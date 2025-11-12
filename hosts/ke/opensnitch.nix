2e4c7ab41b58cef5c96f76cff938f68cc99566bd
Kenny MacDermid <kenny@macdermid.ca>
2025-11-12 14:02:20 -0400

    Use getExe in opensnitch and sort entires

--- hosts/ke/opensnitch.nix
+++ hosts/ke/opensnitch.nix
@@ -12,11 +12,11 @@
     rules = with pkgs;
       lib.mkMerge [
         {
-          signal = lib.snitchAllowPath "${lib.getBin signal-desktop-bin}/lib/signal-desktop/signal-desktop";
-          keybase = lib.snitchAllowPath "${lib.getBin keybase}/bin/keybase";
-          kbfs = lib.snitchAllowPath "${lib.getBin kbfs}/bin/kbfsfuse";
-          nix = lib.snitchAllowPath "${lib.getBin nix}/bin/nix";
           firefox = lib.snitchAllowPath "${lib.getBin firefox}/lib/firefox/firefox";
+          kbfs = lib.snitchAllowPath (lib.getExe' kbfs "kbfsfuse");
+          keybase = lib.snitchAllowPath (lib.getExe keybase);
+          nix = lib.snitchAllowPath (lib.getExe nix);
+          signal = lib.snitchAllowPath "${lib.getBin signal-desktop-bin}/lib/signal-desktop/signal-desktop";
           systemd-networkd = lib.snitchAllowPath "${lib.getBin pkgs.systemd}/lib/systemd/systemd-networkd";
         }
         (lib.opensnitch.mkSimpleRules [
@@ -26,30 +26,51 @@
             port = 53;
           }
           {
-            name = "Allow Systemd DNS";
-            process = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
-            port = 53;
-          }
-          {
-            name = "Allow NTP";
-            process = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
-            protocol = "udp";
-            port = 123;
-          }
-          {
             name = "Allow Avahi";
-            process = "${lib.getBin pkgs.avahi}/bin/avahi-daemon";
+            process = lib.getExe' avahi "avahi-daemon";
             protocol = "udp";
             port = 5353;
           }
           {
-            name = "Allow Zerotier";
-            process = "${lib.getBin zerotierone}/bin/zerotier-one";
-            protocol = "udp";
+            name = "Allow Fwupd Updates";
+            process = lib.getExe' fwupd "fwupdmgr";
+            host = "cdn.fwupd.org";
+            protocol = "tcp";
+            port = 443;
           }
           {
             name = "Allow Keybase";
-            process = "${lib.getBin keybase}/bin/keybase";
+            process = lib.getExe keybase;
+          }
+          {
+            name = "Allow msmtp magadu";
+            process = lib.getExe msmtp.binaries;
+            host = "smtp.migadu.com";
+            protocol = "tcp";
+            port = 587;
+          }
+          {
+            name = "Allow msmtp google";
+            process = lib.getExe msmtp.binaries;
+            host = "smtp.gmail.com";
+            protocol = "tcp";
+            port = 587;
+          }
+          {
+            name = "Allow NTP";
+            process = "${lib.getBin systemd}/lib/systemd/systemd-timesyncd";
+            protocol = "udp";
+            port = 123;
+          }
+          {
+            name = "Allow Systemd DNS";
+            process = "${lib.getBin systemd}/lib/systemd/systemd-resolved";
+            port = 53;
+          }
+          {
+            name = "Allow Zerotier";
+            process = lib.getExe' zerotierone "zerotier-one";
+            protocol = "udp";
           }
         ])
       ];
