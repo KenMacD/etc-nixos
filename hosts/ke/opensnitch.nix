5ff97f4a5480cdd317824d66603955c674dcbc9d
Kenny MacDermid <kenny@macdermid.ca>
2025-12-18 12:40:35 -0400

    Add more opensnitch rules

--- hosts/ke/opensnitch.nix
+++ hosts/ke/opensnitch.nix
@@ -2,7 +2,10 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  smtpHosts = ["smtp.migadu.com" "smtp.gmail.com"];
+  imapHosts = ["imap.migadu.com" "imap.gmail.com"];
+in {
   home-manager.users.kenny = {
     services.opensnitch-ui.enable = true;
   };
@@ -11,6 +14,7 @@
     enable = true;
     rules = with pkgs;
       lib.mkMerge [
+        # Path-based rules
         {
           firefox = lib.snitchAllowPath "${lib.getBin firefox}/lib/firefox/firefox";
           kbfs = lib.snitchAllowPath (lib.getExe' kbfs "kbfsfuse");
@@ -19,60 +23,66 @@
           signal = lib.snitchAllowPath "${lib.getBin signal-desktop-bin}/lib/signal-desktop/signal-desktop";
           systemd-networkd = lib.snitchAllowPath "${lib.getBin pkgs.systemd}/lib/systemd/systemd-networkd";
         }
-        (lib.opensnitch.mkSimpleRules [
-          {
-            name = "Local DNS";
-            ip = "127.0.0.53";
-            port = 53;
-          }
-          {
-            name = "Allow Avahi";
-            process = lib.getExe' avahi "avahi-daemon";
-            protocol = "udp";
-            port = 5353;
-          }
-          {
-            name = "Allow Fwupd Updates";
-            process = lib.getExe' fwupd "fwupdmgr";
-            host = "cdn.fwupd.org";
-            protocol = "tcp";
-            port = 443;
-          }
-          {
-            name = "Allow Keybase";
-            process = lib.getExe keybase;
-          }
-          {
-            name = "Allow msmtp magadu";
+        # Simple network rules
+        (
+          lib.opensnitch.mkSimpleRules [
+            {
+              name = "Local DNS";
+              ip = "127.0.0.53";
+              port = 53;
+            }
+            {
+              name = "Allow Avahi";
+              process = lib.getExe' avahi "avahi-daemon";
+              protocol = "udp";
+              port = 5353;
+            }
+            {
+              name = "Allow Firmware Updates";
+              process = lib.getExe' fwupd ".fwupdmgr-wrapped";
+              host = "cdn.fwupd.org";
+              protocol = "tcp";
+              port = 443;
+            }
+            {
+              name = "Allow Keybase";
+              process = lib.getExe keybase;
+            }
+            {
+              name = "Allow NTP";
+              process = "${lib.getBin systemd}/lib/systemd/systemd-timesyncd";
+              protocol = "udp";
+              port = 123;
+            }
+            {
+              name = "Allow Systemd DNS";
+              process = "${lib.getBin systemd}/lib/systemd/systemd-resolved";
+              port = 53;
+            }
+            {
+              name = "Allow Zerotier";
+              process = lib.getExe' zerotierone "zerotier-one";
+              protocol = "udp";
+            }
+          ]
+        )
+        # Generate email rules for each host
+        (lib.opensnitch.mkSimpleRules (map (host: {
+            name = "Allow msmtp ${host}";
             process = lib.getExe msmtp.binaries;
-            host = "smtp.migadu.com";
+            host = host;
             protocol = "tcp";
             port = 587;
-          }
-          {
-            name = "Allow msmtp google";
-            process = lib.getExe msmtp.binaries;
-            host = "smtp.gmail.com";
+          })
+          smtpHosts))
+        (lib.opensnitch.mkSimpleRules (map (host: {
+            name = "Allow fdm ${host}";
+            process = lib.getExe' fdm "fdm";
+            host = host;
             protocol = "tcp";
-            port = 587;
-          }
-          {
-            name = "Allow NTP";
-            process = "${lib.getBin systemd}/lib/systemd/systemd-timesyncd";
-            protocol = "udp";
-            port = 123;
-          }
-          {
-            name = "Allow Systemd DNS";
-            process = "${lib.getBin systemd}/lib/systemd/systemd-resolved";
-            port = 53;
-          }
-          {
-            name = "Allow Zerotier";
-            process = lib.getExe' zerotierone "zerotier-one";
-            protocol = "udp";
-          }
-        ])
+            port = 993;
+          })
+          imapHosts))
       ];
   };
 }
