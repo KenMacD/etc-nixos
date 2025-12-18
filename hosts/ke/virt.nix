cd0232ef4f3ed84a4bbfd742f0cb052de7bdc1c2
Kenny MacDermid <kenny@macdermid.ca>
2025-12-18 12:45:08 -0400

    Update ovmf paths

--- hosts/ke/virt.nix
+++ hosts/ke/virt.nix
@@ -74,10 +74,17 @@
       short-name-mode="enforcing"
     '';
 
+    # /run/libvirt/nix-ovmf/edk2-x86_64-code.fd
+    "ovmf/edk2-x86_64-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-code.fd";
+    };
+
+    # /run/libvirt/nix-ovmf/edk2-x86_64-secure-code.fd
     "ovmf/edk2-x86_64-secure-code.fd" = {
       source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
     };
 
+    # /run/libvirt/nix-ovmf/edk2-i386-vars.fd
     "ovmf/edk2-i386-vars.fd" = {
       source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
     };
