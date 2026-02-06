e2fff780e4790c9908114b403f53d893ec6714a5
Kenny MacDermid <kenny@macdermid.ca>
2026-02-06 16:16:35 -0400

    Update leap keymaps to use vim.keymap.set
    leap.set_default_mappings() was removed upstream

--- common.nix
+++ common.nix
@@ -320,7 +320,8 @@ with lib; {
             },
           }
           -- 's' then two chars to jump to that
-          require('leap').set_default_mappings()
+          vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+          vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
 
           local cmp = require('cmp')
           local luasnip = require('luasnip')
