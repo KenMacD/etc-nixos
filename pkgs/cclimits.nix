a0bdaf0511794c861c3d88bb3d574e66dce41003
Kenny MacDermid <kenny@macdermid.ca>
2026-07-06 20:39:04 -0300

    Bump cclimits to v1.2.12

--- pkgs/cclimits.nix
+++ pkgs/cclimits.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "cclimits";
-  version = "v1.2.9";
+  version = "v1.2.12";
 
   src = fetchFromGitHub {
     owner = "cruzanstx";
     repo = "cclimits";
     rev = version;
-    hash = "sha256-jBDYDWt4bB4OFGbxbLBtsKywflqqOBEQ/4eLm6XZhD8=";
+    hash = "sha256-9svkDDgXkI/Ie2pGMFqio1sIi+QOBgY0sdMb5CaBtU0=";
   };
 
   buildInputs = [python3];
